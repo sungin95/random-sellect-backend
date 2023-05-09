6f98e6879e67d6e423dbd965fbf17c0095268454
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-05-09 11:39:48 +0900

    feat: STATIC_ROOT 추가

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +144,5 @@ CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
