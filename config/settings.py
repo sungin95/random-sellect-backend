fa8f0b7b3334a88a64f00221eee52ec56e6303b5
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-05-11 16:20:53 +0900

    feat: 배포환경에서 프론트 백엔드 연결 작업

--- config/settings.py
+++ config/settings.py
@@ -139,11 +139,11 @@ PAGE_SIZE = 10
 # Auth
 AUTH_USER_MODEL = "users.User"
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = [".gpt-is-interviewer.me"]
+CSRF_TRUSTED_ORIGINS = [".gpt-is-interviewer.me"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
