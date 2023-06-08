77a3821a5e51fad7074201150b35ffae257338d5
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-08 15:42:30 +0900

    test: 로그인 안한 상황 get total갯수

--- questions/tests.py
+++ questions/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from users.models import User
+from .models import *
 
-# Create your tests here.
+
+# 로그인 안한상황에서 생성
+class TestQuestions(APITestCase):
+    URL = "/api/v1/questions/"
+
+    def test_total_count_num(self):
+        response = self.client.get(self.URL + "total")
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status code isn't 200.",
+        )
