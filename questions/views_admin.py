28cd309ae1f0808b85d123389e787265f45e1f83
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-08-08 16:46:07 +0900

    style: views_admin 주석처리 - 현재 쓸 일 없어서 그냥 주석 처리 - 다음에 비슷한 일 있을때 참고해서 삭제는 안함.

--- questions/views_admin.py
+++ questions/views_admin.py
@@ -1,35 +1,35 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Questions, SellectedQuestions
-from rest_framework import status
-from rest_framework.exceptions import ParseError
-from django.db import transaction
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import Questions, SellectedQuestions
+# from rest_framework import status
+# from rest_framework.exceptions import ParseError
+# from django.db import transaction
 
 
-# 새로 추가된 모델에 적절한 값을 넣어주기 위한 함수
-class ConnectQuestion(APIView):
-    @transaction.atomic(using="default")
-    def get(self, request):
-        if request.user.is_staff == True:
-            try:
-                with transaction.atomic():
-                    sellected_questions = SellectedQuestions.objects.all()
-                    for sq in sellected_questions:
-                        qs = Questions.objects.filter(description=sq.description)
-                        # 1 값 넣어줌
-                        if len(qs) == 1:
-                            sq.question = qs[0]
-                        # 2 없으면 None
-                        elif len(qs) == 0:
-                            sq.question = None
-                        # 3 중복되면 에러 발생(롤백한다. )
-                        else:
-                            raise ParseError
-                        sq.save()
-                    return Response(status.HTTP_200_OK)
-            except:
-                pass
-            return Response(
-                {"message": "작업도중 에러 발생"}, status=status.HTTP_400_BAD_REQUEST
-            )
-        return Response({"message": "스태프가 아님"}, status=status.HTTP_403_FORBIDDEN)
+# # 새로 추가된 모델에 적절한 값을 넣어주기 위한 함수
+# class ConnectQuestion(APIView):
+#     @transaction.atomic(using="default")
+#     def get(self, request):
+#         if request.user.is_staff == True:
+#             try:
+#                 with transaction.atomic():
+#                     sellected_questions = SellectedQuestions.objects.all()
+#                     for sq in sellected_questions:
+#                         qs = Questions.objects.filter(description=sq.description)
+#                         # 1 값 넣어줌
+#                         if len(qs) == 1:
+#                             sq.question = qs[0]
+#                         # 2 없으면 None
+#                         elif len(qs) == 0:
+#                             sq.question = None
+#                         # 3 중복되면 에러 발생(롤백한다. )
+#                         else:
+#                             raise ParseError
+#                         sq.save()
+#                     return Response(status.HTTP_200_OK)
+#             except:
+#                 pass
+#             return Response(
+#                 {"message": "작업도중 에러 발생"}, status=status.HTTP_400_BAD_REQUEST
+#             )
+#         return Response({"message": "스태프가 아님"}, status=status.HTTP_403_FORBIDDEN)
