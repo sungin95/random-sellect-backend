7d840a6d35c3fd84163bbcd86978c013ac41237d
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-08-21 16:53:57 +0900

    fix: ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     ".gpt-is-interviewer.me",
-    ".ap-northeast-2.compute.amazonaws.com",
+    "ec2-43-201-82-16.ap-northeast-2.compute.amazonaws.com",
 ]
 
 
